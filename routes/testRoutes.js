d28c83be99670c9f413597382a07c5cf9ee6986a
Pushker Prakash <pprakash.1st@gmail.com>
2024-10-13 23:52:28 +0530

    further testing

--- routes/testRoutes.js
+++ routes/testRoutes.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 router.get('/hello', (req, res) => {
   console.log("hello");
-  res.send('Hello World!');
+  res.send('teri maka bhosda mvc architecture');
 });
 
 module.exports = router;
