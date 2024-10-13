cd2a8e758b99baffda1633e9d12b2e052386ca45
Pushker Prakash <pprakash.1st@gmail.com>
2024-10-14 00:18:05 +0530

    added cors policy

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const cors = require('cors'); 
 const connectDB = require('./config/db');  // MongoDB connection
 const chatbotRoutes = require('./routes/chatbotRoutes');
 
